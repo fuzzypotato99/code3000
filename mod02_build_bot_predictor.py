3fb50a64f2db49c901ac50b224248660d70ecb7c
Seth Robbin <sethrobbin2005@gmail.com>
2026-09-15 15:27:36 -0400

    Testing

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,8 +10,8 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
+        learning_rate=0.05,
+        n_estimators=200,
         max_depth=8,
         subsample=1,
         min_samples_leaf=1,
