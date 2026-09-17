1736b54b8e6d7d2a38d6458c6f0b88b9b17438f1
fuzzypotato99 <sethrobbin2005@gmail.com>
2026-09-16 22:23:48 -0400

    s

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -13,8 +13,8 @@ def train_model(X, y, seed=seed):
         learning_rate=0.1,
         n_estimators=280,
         max_depth=12,
-        subsample=.8,
-        min_samples_leaf=1,
+        subsample=1,
+        min_samples_leaf=2,
         random_state=seed
     )
     model.fit(X, y)
