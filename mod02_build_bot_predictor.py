9a3f560ac6f91ca135eaadc8d750dceebee88d95
fuzzypotato99 <sethrobbin2005@gmail.com>
2026-09-16 22:25:34 -0400

    d

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
+        learning_rate=0.05,
         n_estimators=280,
         max_depth=12,
         subsample=1,
-        min_samples_leaf=2,
+        min_samples_leaf=1,
         random_state=seed
     )
     model.fit(X, y)
